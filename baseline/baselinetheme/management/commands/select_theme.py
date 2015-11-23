fa8889622c4c306bc9e0ce3193a4cb52133d5895
Casey Kinsey <casey.kinsey@gmail.com>
2015-11-23 09:46:05 -0600

    Restart app on theme change

--- baseline/baselinetheme/management/commands/select_theme.py
+++ baseline/baselinetheme/management/commands/select_theme.py
@@ -1,5 +1,7 @@
 import json
 import os
+import subprocess
+
 from django.conf import settings
 from django.core.management import BaseCommand, CommandError
 
@@ -34,3 +36,11 @@ class Command(BaseCommand):
         # Write the new configuration
         with open(os.path.join(settings.BASE_DIR, '.bl-theme'), 'w') as theme_conf:
             theme_conf.write(json.dumps(new_conf))
+
+        # Restart the app
+        if settings.DEBUG:
+            subprocess.call(['touch', os.path.join(settings.PROJECT_DIR, 'wsgi.py')],
+                            env=os.environ.copy())
+        else:
+            # TODO: Integrate hooks to restart uWSGI
+            pass
