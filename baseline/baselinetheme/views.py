49f52d0c81b1ba7c3c28aeec40742e58ae5987a1
Casey Kinsey <casey.kinsey@gmail.com>
2015-11-28 20:53:29 -0600

    Adds basic theme selection page to wagtail admin

--- baseline/baselinetheme/views.py
+++ baseline/baselinetheme/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+import time
 
-# Create your views here.
+from django.conf import settings
+from django.contrib import messages
+from django.core.management import call_command, CommandError
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, RedirectView
+
+
+class ThemeIndex(TemplateView):
+    """
+    Admin view to display all themes and allow them to be activated, uninstalled by the user.
+    """
+    template_name = "baselinetheme/theme_index.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({
+            'installed_themes': settings.THEME_CONFIG['installed'],
+            'active_theme': settings.THEME_CONFIG['active']
+        })
+
+        return super(ThemeIndex, self).get_context_data(**kwargs)
+
+
+class ActivateTheme(RedirectView):
+    permanent = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        """
+        Direct back to the theme page
+        """
+        return reverse('baseline-theme-index')
+
+    def get(self, request, *args, **kwargs):
+        """
+        Activate the selected theme.
+        """
+        theme = request.GET.get('theme')
+        try:
+            call_command('select_theme', *[theme])
+            messages.success(request, "Theme {theme} was activated successfully.".format(
+                theme=theme))
+        except CommandError:
+            messages.error(request, "There was an error activating {theme}.".format(
+                theme=theme))
+
+        return super(ActivateTheme, self).get(request, *args, **kwargs)
---
+++ baseline/baselinetheme/wagtail_hooks.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from django.core.urlresolvers import reverse
+
+from wagtail.wagtailcore import hooks
+from wagtail.wagtailadmin.menu import MenuItem
+
+from baseline.baselinetheme.views import ActivateTheme, ThemeIndex
+
+
+@hooks.register('register_admin_menu_item')
+def theme_menu():
+    return MenuItem('Themes', reverse('baseline-theme-index'), classnames='icon icon-edit',
+                    order=10000)
+
+@hooks.register('register_admin_urls')
+def theme_urls():
+    return [
+        url(r'^themes/$', ThemeIndex.as_view(), name='baseline-theme-index'),
+        url(r'^themes/activate/$', ActivateTheme.as_view(), name='baseline-theme-activate'),
+    ]
