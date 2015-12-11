14dccb9bf85a7d575cd54d713749528421d6240d
Casey Kinsey <casey.kinsey@gmail.com>
2015-12-11 15:16:51 -0600

    Port startbootstrap theme for baseline default theme

--- baseline/baselinecore/models/home.py
+++ baseline/baselinecore/models/home.py
@@ -1,9 +1,17 @@
 from __future__ import unicode_literals
 
+from django.db import models
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+
 from baseline.baselinecore.models.base import BaselinePage
 
 class HomePage(BaselinePage):
     """
     Homepage specific configuration
     """
-    pass
+
+    snippet = models.CharField(max_length=2502, default="Welcome to Baseline!")
+
+    content_panels = BaselinePage.content_panels + [
+        FieldPanel('snippet', classname='title'),
+    ]
